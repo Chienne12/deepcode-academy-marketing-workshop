8b002d86f265ae22f1e250eb0789f1b004041060
Chienne12 <phungtrongchien102@gmail.com>
2026-03-31 19:42:29 +0700

    Update vite.config.ts
    fix lỗi deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
-  const repoName = 'deepcode-academy-marketing-workshop';
 
   return {
-    base: mode === 'production' ? `/${repoName}/` : '/',
+    base: mode === 'production' ? './' : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
